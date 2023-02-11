3659825703bc559306fc07aa467e5a4ef0633848
gnzlplcs <palaciosgm@outlook.com>
2023-02-10 19:17:23 -0500

    :tada: first commit
    init project: tohto imports

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+      <h1>Tohto Imports</h1>
+    </div>
+  )
+}
+
+export default App
