46a2ad45e0a42cbc061ccc5182a10b2303fc4f05
gnzlplcs <palaciosgm@outlook.com>
2023-02-11 12:57:21 -0500

    :rocket: apply Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import Header from "./components/Header";
 import Form from "./components/Form";
-import logoImg from "./assets/logo003-7.svg";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Página en construcción</h1>
-      <img src={logoImg} alt="Tohto Lightning main image" />
       <Form />
     </div>
   );
